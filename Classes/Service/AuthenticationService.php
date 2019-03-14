78fb955f294949b87b293fcc5d4bfeeb45acaec7
Jan Runte <jan.runte@hmmh.de>
2019-03-14 16:47:14 +0100

    remove var_dump

--- Classes/Service/AuthenticationService.php
+++ Classes/Service/AuthenticationService.php
@@ -8,10 +8,9 @@ class AuthenticationService extends \TYPO3\CMS\Sv\AuthenticationService
 {
     public function getUser()
     {
-        var_dump(PHP_SAPI);
-        if (\TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()->isDevelopment() && ('cli' !== PHP_SAPI))
-        {
+        if (\TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()->isDevelopment() && ('cli' !== PHP_SAPI)) {
             $autoLoginUserName = trim(getenv('TYPO3_AUTOLOGIN_USER'));
+
             return $this->fetchUserRecord($autoLoginUserName);
         } else {
             return false;
