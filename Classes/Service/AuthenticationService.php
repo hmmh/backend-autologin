7a55014b979c17607d46299a7f58170efc70f933
Jan Runte <jan.runte@hmmh.de>
2019-03-22 17:26:15 +0100

    add support for cookies and get settings

--- Classes/Service/AuthenticationService.php
+++ Classes/Service/AuthenticationService.php
@@ -2,12 +2,29 @@
 
 namespace HMMH\BeAutoLogin\Service;
 
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Class AuthenticationService
+ *
+ */
 class AuthenticationService extends \TYPO3\CMS\Sv\AuthenticationService
 {
+    const COOKIE_NAME = 'TYPO3_AUTOLOGIN_USER';
+
+    /**
+     * @return bool
+     */
     public function getUser()
     {
-        if (\TYPO3\CMS\Core\Utility\GeneralUtility::getApplicationContext()->isDevelopment() && ('cli' !== PHP_SAPI)) {
-            $autoLoginUserName = trim(getenv('TYPO3_AUTOLOGIN_USER'));
+        if (GeneralUtility::getApplicationContext()->isDevelopment() && ('cli' !== PHP_SAPI)) {
+            $autoLoginUserName = trim(
+                GeneralUtility::_GET(static::COOKIE_NAME) ?? $_COOKIE[static::COOKIE_NAME] ?? getenv(static::COOKIE_NAME)
+            );
+
+            if ((empty($_COOKIE[static::COOKIE_NAME])) || ($_COOKIE[static::COOKIE_NAME] !== $autoLoginUserName)) {
+                setcookie(static::COOKIE_NAME, $autoLoginUserName);
+            }
 
             return $this->fetchUserRecord($autoLoginUserName);
         } else {
